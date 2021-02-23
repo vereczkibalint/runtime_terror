26f26faa9f9857f05f767815adbd267eca8170d4
somogyidavid <ksomogyi1515@gmail.com>
2021-02-23 17:02:05 +0100

    Swagger configuration added

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder().setTitle('AFP 2021 - Money Tracker').setDescription('Money tracking web application for everyday use.').setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
