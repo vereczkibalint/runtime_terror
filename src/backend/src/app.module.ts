75a9e98c0ea081832c93355e2a6fc9dfad2f2ed6
somogyidavid <ksomogyi1515@gmail.com>
2021-02-23 16:06:30 +0100

    MongoDB connection established

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypegooseModule } from "nestjs-typegoose";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env'
+  }), TypegooseModule.forRoot(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
