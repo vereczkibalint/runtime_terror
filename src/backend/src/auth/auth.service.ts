89f8c652fd20f2ebb7afb16199cfd2b13ab24545
somogyidavid <ksomogyi1515@gmail.com>
2021-03-02 16:29:33 +0100

    Auth service implemented User findByEmail added

--- src/backend/src/auth/auth.service.ts
+++ src/backend/src/auth/auth.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { AuthLoginDto } from './dto/AuthLoginDto';
+import { UserDto } from './dto/UserDto';
+import { AuthenticatedDto } from './dto/AuthenticatedDto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UsersService, private readonly jwtService: JwtService) {}
+
+  async login(authLoginDto: AuthLoginDto): Promise<AuthenticatedDto> {
+    const user = await this.userService.findByEmail(authLoginDto.email);
+
+    if(!user) {
+      throw new HttpException('Nincs felhasználó ilyen email címmel!', HttpStatus.UNAUTHORIZED);
+    }
+
+    const passwordsAreMatching = await bcrypt.compare(authLoginDto.password, user.password);
+
+    if(!passwordsAreMatching) {
+      throw new HttpException('Sikertelen bejelentkezés!', HttpStatus.UNAUTHORIZED);
+    }
+
+    const userDto: UserDto = {
+      _id: user._id,
+      lastName: user.lastName,
+      firstName: user.firstName,
+      email: user.email,
+      lastLogin: user.lastLogin,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
+    };
+
+    const token = this._createToken(userDto);
+
+    return {
+      token,
+      user: userDto
+    }
+  }
+
+
+  private _createToken(user: UserDto): string {
+    return this.jwtService.sign(user);
+  }
+}
--- src/backend/src/users/users.service.ts
+++ src/backend/src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import User from "./schemas/user.schema";
+import { InjectModel } from "nestjs-typegoose";
+import { ReturnModelType } from "@typegoose/typegoose";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private readonly userModel: ReturnModelType<typeof User>) {}
+
+  async findByEmail(email: string): Promise<User> {
+    return await this.userModel.findOne({ email }).exec();
+  }
+}
