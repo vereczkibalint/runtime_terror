669d81c6489dfa21d876ee91d4b6534861494fc6
vereczkibalint <vereczkibalint@gmail.com>
2021-03-02 16:34:44 +0100

    login implemented

--- src/backend/src/auth/auth.controller.ts
+++ src/backend/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthLoginDto } from "./dto/AuthLoginDto";
+import { AuthenticatedDto } from "./dto/AuthenticatedDto";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  async login(@Body() loginDto: AuthLoginDto): Promise<AuthenticatedDto> {
+    return await this.authService.login(loginDto);
+  }
+}
