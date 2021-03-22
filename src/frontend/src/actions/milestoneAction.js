dab4fb3f819265c6d970a2d10ffadcb294a035b3
zsolti1012 <56861547+zsolti1012@users.noreply.github.com>
2021-03-22 17:03:37 +0100

    Update milestoneAction.js
    Import components and create dummy data

---
+++ src/frontend/src/actions/milestoneAction.js
@@ -0,0 +1,65 @@
+import {
+  GET_MILESTONES,
+  ADD_MILESTONE,
+  UPDATE_MILESTONE,
+  DELETE_MILESTONE,
+  SET_CURRENT_MILESTONE,
+  CLEAR_CURRENT_MILESTONE,
+  MILESTONE_ERROR,
+  SET_LOADING
+} from './types';
+
+// Dummy Data
+//TODO: Delete when backend is finished
+let data = {
+  milestones: [
+    {
+      id: 1,
+      owner: 'Feri',
+      name: 'PS5',
+      goalPrice: 185000,
+      deadLine: new Date().setFullYear(2022),
+      sources: [
+        {
+          account: 'main',
+          amount: 3000,
+          createdAt: new Date().setMonth(3)
+        },
+        {
+          account: 'second',
+          amount: 2000,
+          createdAt: new Date().setMonth(1)
+        },
+        {
+          account: 'asdfasdfasdf',
+          amount: 30000000,
+          createdAt: new Date().setMonth(11)
+        }
+      ]
+    },
+    {
+      id: 2,
+      owner: 'Zsolt',
+      name: 'PC',
+      goalPrice: 200000,
+      deadLine: new Date().setFullYear(2023),
+      sources: [
+        {
+          account: '123',
+          amount: 2222,
+          createdAt: new Date().setMonth(1)
+        },
+        {
+          account: 'xyz',
+          amount: 1111,
+          createdAt: new Date().setMonth(1)
+        },
+        {
+          account: 'asdfasdfasdf',
+          amount: 4444,
+          createdAt: new Date().setMonth(4)
+        }
+      ]
+    }
+  ]
+};
