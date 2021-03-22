eea12862cbc5fbfce4c83bb486365ab26ec75ac6
zsolti1012 <56861547+zsolti1012@users.noreply.github.com>
2021-03-22 17:33:56 +0100

    Update Milestones.js
    Create View

--- src/frontend/src/components/milestones/Milestones.js
+++ src/frontend/src/components/milestones/Milestones.js
@@ -4,3 +4,44 @@ import MilestoneItem from './MilestoneItem';
 import { connect } from 'react-redux';
 import { getMilestones } from '../../actions/milestoneAction';
 import PropTypes from 'prop-types';
+
+const Milestones = ({ milestone: { milestones, loading }, getMilestones }) => {
+  useEffect(() => {
+    getMilestones();
+    //eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      <div className='d-flex justify-content-between'>
+        <h1>Milestones</h1>
+      </div>
+      <button>Cél kitűzése</button>
+
+      {loading || milestones === null ? (
+        <h1>Loading...</h1>
+      ) : (
+        <ul>
+          {milestones !== null && milestones.length !== 0 ? (
+            milestones.map((milestone) => (
+              <MilestoneItem key={milestone.id} milestone={milestone} />
+            ))
+          ) : (
+            <h3>Adjon hozzá új célt</h3>
+          )}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+Milestones.propTypes = {
+  milestone: PropTypes.object.isRequired,
+  getMilestones: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  milestone: state.milestone
+});
+
+export default connect(mapStateToProps, { getMilestones })(Milestones);
