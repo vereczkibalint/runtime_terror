36ce95459a5ee4ae9eb1881614b1ba1b638ae25a
zsolti1012 <56861547+zsolti1012@users.noreply.github.com>
2021-03-22 17:23:35 +0100

    Update milestoneReducer.js
    create ceases

--- src/frontend/src/reducers/milestoneReducer.js
+++ src/frontend/src/reducers/milestoneReducer.js
@@ -15,3 +15,59 @@ const initialState = {
   error: null,
   loading: false
 };
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_MILESTONES:
+      return {
+        ...state,
+        milestones: action.payload,
+        loading: false
+      };
+    case ADD_MILESTONE:
+      return {
+        ...state,
+        milestones: [...state.milestones, action.payload],
+        loading: false
+      };
+    case DELETE_MILESTONE:
+      return {
+        ...state,
+        milestones: state.milestones.filter(
+          (milestone) => milestone.id !== action.payload
+        ),
+        loading: false
+      };
+    case UPDATE_MILESTONE:
+      return {
+        ...state,
+        milestones: state.milestones.map((milestone) =>
+          milestone.id === action.payload.id ? action.payload : milestone
+        ),
+        loading: false
+      };
+    case SET_CURRENT_MILESTONE:
+      return {
+        ...state,
+        current: action.payload
+      };
+    case CLEAR_CURRENT_MILESTONE:
+      return {
+        ...state,
+        current: null
+      };
+    case MILESTONE_ERROR:
+      console.error(action.payload);
+      return {
+        ...state,
+        error: action.payload
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    default:
+      return state;
+  }
+};
