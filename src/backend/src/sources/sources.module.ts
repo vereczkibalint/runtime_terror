855c78d1741b777cc56f1f753fb69274d0b333a0
vereczkibalint <vereczkibalint@gmail.com>
2021-03-23 17:28:06 +0100

    removed source from imports

--- src/backend/src/sources/sources.module.ts
+++ src/backend/src/sources/sources.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SourcesController } from './sources.controller';
 import { SourcesService } from './sources.service';
-import { TypegooseModule } from "nestjs-typegoose";
-import Source from "./schemas/source.schema";
 
 @Module({
-  imports: [TypegooseModule.forFeature([Source])],
   controllers: [SourcesController],
   providers: [SourcesService]
 })
