089d54b501483576d75389bae6035b7d0bb7aec3
zsolti1012 <56861547+zsolti1012@users.noreply.github.com>
2021-03-30 13:00:03 +0200

    Update index.js
    Add incomeReducer

--- src/frontend/src/reducers/index.js
+++ src/frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import milestoneReducer from './milestoneReducer';
 import expendureReducer from './expenditureReducer';
+import incomeReducer from './incomeReducer';
 
 export default combineReducers({
   milestone: milestoneReducer,
-  expenditure:expendureReducer
+  expenditure:expendureReducer,
+  income:incomeReducer
 });
