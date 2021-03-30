f119ce36fe46ba7f3b1dad4620638c808d25da5b
vereczkibalint <vereczkibalint@gmail.com>
2021-03-30 15:55:14 +0200

    fixed typegoose module import

--- src/backend/src/milestones/milestones.module.ts
+++ src/backend/src/milestones/milestones.module.ts
@@ -3,9 +3,10 @@ import { MilestonesController } from './milestones.controller';
 import { MilestonesService } from './milestones.service';
 import { TypegooseModule } from "nestjs-typegoose";
 import Milestone from "./schemas/milestone.schema";
+import Source from "./schemas/source.schema";
 
 @Module({
-  imports: [TypegooseModule.forFeature([Milestone])],
+  imports: [TypegooseModule.forFeature([Milestone, Source])],
   controllers: [MilestonesController],
   providers: [MilestonesService]
 })
