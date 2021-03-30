2ca2729a3279a0582384026b6adba375a2f00ad2
zsolti1012 <56861547+zsolti1012@users.noreply.github.com>
2021-03-30 16:58:06 +0200

    Update index.js
    add auth:auth

--- src/frontend/src/reducers/index.js
+++ src/frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import milestoneReducer from './milestoneReducer';
 import expendureReducer from './expenditureReducer';
 import incomeReducer from './incomeReducer';
+import auth from './auth';
 
 export default combineReducers({
   milestone: milestoneReducer,
   expenditure:expendureReducer,
-  income:incomeReducer
+  income:incomeReducer,
+  auth:auth
 });
