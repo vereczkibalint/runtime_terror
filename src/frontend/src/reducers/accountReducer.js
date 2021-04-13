e54f847ec1ed63210e61a8fc58dd5ec9f0bbed37
fepu08 <ferencbesenyei@protonmail.com>
2021-04-13 21:05:51 +0200

    add accountReducer

---
+++ src/frontend/src/reducers/accountReducer.js
@@ -0,0 +1,75 @@
+import {
+  GET_ACCOUNT,
+  GET_ACCOUNTS,
+  ADD_ACCOUNT,
+  UPDATE_ACCOUNT,
+  DELETE_ACCOUNT,
+  SET_CURRENT_ACCOUNT,
+  CLEAR_CURRENT_ACCOUNT,
+  ACCOUNT_ERROR,
+  SET_LOADING,
+} from "./types";
+
+const initialState = {
+  accounts: [],
+  current: null,
+  loading: false,
+  error: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ACCOUNT:
+      return {
+        ...state,
+        accounts: action.payload,
+        loading: false,
+      };
+    case GET_ACCOUNTS:
+      return {
+        ...state,
+        accounts: action.payload,
+        loading: false,
+      };
+    case ADD_ACCOUNT:
+      return {
+        ...state,
+        accounts: [...state.accounts, action.payload],
+        loading: false,
+      };
+    case DELETE_ACCOUNT:
+      return {
+        ...state,
+        account: state.accounts.filter(
+          (account) => account.id !== action.payload
+        ),
+        loading: false,
+      };
+    case UPDATE_ACCOUNT:
+      return {
+        ...state,
+        accounts: state.accounts.map((account) =>
+          account.id === action.payload.id ? action.payload : account
+        ),
+        loading: false,
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ACCOUNT_ERROR:
+      console.error(action.payload);
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
