533cbb99831028e1ab8372aed2d6b81b6838b83d
fepu08 <ferencbesenyei@protonmail.com>
2021-04-15 15:05:30 +0200

    add accounts and account form components

--- src/frontend/src/components/accounts/AccountForm.js
+++ src/frontend/src/components/accounts/AccountForm.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { Modal, Button, Form } from "react-bootstrap";
 
-const AccountForm = () => {
-  return <div>Account Form</div>;
+const AccountForm = ({ show, handleClose }) => {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add new account</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Name" />
+          </Form.Group>
+
+          <Form.Group controlId="color">
+            <Form.Label>Color</Form.Label>
+            <Form.Control type="color" title="Choose your color" />
+          </Form.Group>
+
+          <Form.Group controlId="balance">
+            <Form.Label>Balance</Form.Label>
+            <Form.Control type="number" value="0" />
+          </Form.Group>
+
+          <hr />
+
+          <div className="d-flex justify-content-between">
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
 };
 
 export default AccountForm;
--- src/frontend/src/components/accounts/Accounts.jsx
+++ src/frontend/src/components/accounts/Accounts.jsx
@@ -1,13 +1,16 @@
-import React, { Fragment, useEffect } from "react";
-import AccountItem from "./AccountItem";
+import React, { Fragment, useEffect, useState } from "react";
 import AccountForm from "./AccountForm";
 import { connect } from "react-redux";
 import { getAccounts } from "../../actions/accountActions";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import { PREFIX } from "../../config";
+import { Button } from "react-bootstrap";
 
 const Accounts = ({ accounts: { accounts, current }, getAccounts }) => {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleClose = () => setShowForm(false);
+  const handleShow = () => setShowForm(true);
+
   useEffect(() => {
     getAccounts();
     //eslint-disable-next-line
@@ -15,8 +18,16 @@ const Accounts = ({ accounts: { accounts, current }, getAccounts }) => {
 
   return (
     <div>
-      <h1>Accounts</h1>
-      {accounts ? (
+      <div className="d-flex justify-content-between my-3">
+        <h1>Accounts</h1>
+        <Button variant="success" onClick={handleShow}>
+          Add Account
+        </Button>
+      </div>
+
+      <AccountForm show={showForm} handleClose={handleClose} />
+
+      {accounts && accounts.length > 0 ? (
         <ul>
           {accounts.map((account) => (
             <Fragment>
@@ -29,7 +40,7 @@ const Accounts = ({ accounts: { accounts, current }, getAccounts }) => {
           ))}
         </ul>
       ) : (
-        <h1>No account to show</h1>
+        <h3>No account to show</h3>
       )}
       <hr />
       <AccountForm />
