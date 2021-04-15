f073597455d24476872fbab95157e4db6596ee08
fepu08 <ferencbesenyei@protonmail.com>
2021-04-15 13:46:24 +0200

    set button group in navbar

--- src/frontend/src/components/layout/Navbar.js
+++ src/frontend/src/components/layout/Navbar.js
@@ -1,17 +1,12 @@
-import React, { Fragment, useState, useEffect } from "react";
-import { NavLink, useHistory } from "react-router-dom";
+import React, { Fragment } from "react";
+import { NavLink, Link, useHistory } from "react-router-dom";
 import { PREFIX } from "../../config";
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup, DropdownButton, Dropdown } from "react-bootstrap";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { checkIsAuthenticated } from "../../utils/authentication";
 import { logout } from "../../actions/authActions";
-
 const Navbar = ({ auth, logout }) => {
-  const [expenditures, setExpenditure] = useState(false);
-  const [incomes, setIncomes] = useState(false);
-  const [stats, setStats] = useState(false);
-  const [milestones, setMilestones] = useState(false);
   const history = useHistory();
 
   const onLogout = () => {
@@ -19,191 +14,102 @@ const Navbar = ({ auth, logout }) => {
     history.push(`${PREFIX}/login`);
   };
 
-  const setToggleMenuStatesToFalse = () => {
-    setExpenditure(false);
-    setIncomes(false);
-    setStats(false);
-    setMilestones(false);
-  };
-
   return (
     <nav id="sidebar" className="bg-primary text-light">
       <div className="sidebar-header">
         <h3>Menü</h3>
       </div>
-
       <div className="d-flex flex-column">
-        <Button className="my-1" as={NavLink} to={`${PREFIX}/`} exact>
-          Kezdőlap
-        </Button>
-        {checkIsAuthenticated(auth) ? (
-          <Fragment>
-            <Button
-              className="my-1"
-              as={NavLink}
-              to={`${PREFIX}/accounts`}
-              exact
-            >
-              Számlák
-            </Button>
-            <Button
-              className="my-1"
-              onClick={() => {
-                setToggleMenuStatesToFalse();
-                setExpenditure(!expenditures);
-              }}
-              active={expenditures}
-            >
-              Kiadás <i className="fas fa-caret-down"></i>
-            </Button>
-            {expenditures && (
-              <div className="d-flex flex-column bg-dark">
-                <Button
-                  variant="dark"
-                  as={NavLink}
-                  exact
-                  to={`${PREFIX}/expenditures`}
-                >
+        <ButtonGroup vertical className="my-3">
+          <Button className="my-1" as={NavLink} to={`${PREFIX}/`} exact>
+            Kezdőlap
+          </Button>
+          {checkIsAuthenticated(auth) ? (
+            <>
+              <Button
+                className="my-1"
+                as={NavLink}
+                to={`${PREFIX}/accounts`}
+                exact
+              >
+                Számlák
+              </Button>
+
+              <DropdownButton
+                className="my-1"
+                as={ButtonGroup}
+                title="Kiadás"
+                id="bg-vertical-dropdown-1"
+              >
+                <Dropdown.Item as={Link} to="/expenditures">
                   Mindent megjelenít
-                </Button>
-                <Button
-                  variant="dark"
-                  as={NavLink}
-                  exact
-                  to={`${PREFIX}/expenditures/b`}
-                >
-                  Futó hónap
-                </Button>
-                <Button
-                  variant="dark"
-                  as={NavLink}
-                  exact
-                  to={`${PREFIX}/expenditures/c`}
-                >
-                  Előző hónap
-                </Button>
-              </div>
-            )}
+                </Dropdown.Item>
+                <Dropdown.Item>Futó hónap</Dropdown.Item>
+                <Dropdown.Item>Előző hónap</Dropdown.Item>
+              </DropdownButton>
 
-            <Button
-              className="my-1"
-              active={incomes}
-              onClick={() => {
-                setToggleMenuStatesToFalse();
-                setIncomes(!incomes);
-              }}
-            >
-              Bevétel <i className="fas fa-caret-down"></i>
-            </Button>
-            {incomes && (
-              <div className="d-flex flex-column bg-dark">
-                <Button
-                  variant="dark"
-                  as={NavLink}
-                  exact
-                  to={`${PREFIX}/incomes`}
-                >
-                  Mindent megjelenít
-                </Button>
-                <Button
-                  variant="dark"
-                  as={NavLink}
-                  exact
-                  to={`${PREFIX}/incomes/b`}
-                >
-                  Futó hónap
-                </Button>
-                <Button
-                  variant="dark"
-                  as={NavLink}
-                  exact
-                  to={`${PREFIX}/incomes/c`}
-                >
-                  Előző hónap
-                </Button>
-              </div>
-            )}
+              <DropdownButton
+                className="my-1"
+                as={ButtonGroup}
+                title="Bevétel"
+                id="bg-vertical-dropdown-1"
+              >
+                <Dropdown.Item eventKey="1">Mindent megjelenít</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Futó hónap</Dropdown.Item>
+                <Dropdown.Item eventKey="3">Előző hónap</Dropdown.Item>
+              </DropdownButton>
 
-            <Button
-              className="my-1"
-              active={stats}
-              onClick={() => {
-                setToggleMenuStatesToFalse();
-                setStats(!stats);
-              }}
-            >
-              Statisztika <i className="fas fa-caret-down"></i>
-            </Button>
-            {stats && (
-              <div className="d-flex flex-column bg-dark">
-                <Button
-                  variant="dark"
-                  as={NavLink}
-                  exact
-                  to={`${PREFIX}/stats`}
-                >
-                  Futó hónap
-                </Button>
-                <Button
-                  variant="dark"
-                  as={NavLink}
-                  exact
-                  to={`${PREFIX}/stats/previousmonth`}
-                >
-                  Előző hónap
-                </Button>
-
-                <Button
-                  variant="dark"
-                  as={NavLink}
-                  exact
-                  to={`${PREFIX}/stats/other`}
-                >
-                  Egyéb
-                </Button>
-              </div>
-            )}
-
-            <Button
-              active={milestones}
-              onClick={() => {
-                setToggleMenuStatesToFalse();
-                setMilestones(!milestones);
-              }}
-              className="my-1"
-              as={NavLink}
-              to={`${PREFIX}/milestones`}
-              exact
-            >
-              Mérföldkövek
-            </Button>
-            <Button
-              className="my-1"
-              as={NavLink}
-              to={`${PREFIX}/settings`}
-              exact
-            >
-              Beállítások
-            </Button>
-            <Button className="my-1" onClick={onLogout}>
-              Kijelentkezés <i className="fas fa-sign-out-alt"></i>
-            </Button>
-          </Fragment>
-        ) : (
-          <Fragment>
-            <Button
-              className="my-1"
-              as={NavLink}
-              to={`${PREFIX}/register`}
-              exact
-            >
-              Regisztráció <i class="far fa-plus-square"></i>
-            </Button>
-            <Button className="my-1" as={NavLink} to={`${PREFIX}/login`} exact>
-              Bejelentkezés <i class="fas fa-sign-in-alt"></i>
-            </Button>
-          </Fragment>
-        )}
+              <DropdownButton
+                className="my-1"
+                as={ButtonGroup}
+                title="Statisztika"
+                id="bg-vertical-dropdown-1"
+              >
+                <Dropdown.Item eventKey="2">Futó hónap</Dropdown.Item>
+                <Dropdown.Item eventKey="3">Előző hónap</Dropdown.Item>
+                <Dropdown.Item eventKey="3">Egyéb</Dropdown.Item>
+              </DropdownButton>
+              <Button
+                className="my-1"
+                as={NavLink}
+                to={`${PREFIX}/milestones`}
+                exact
+              >
+                Mérföldkövek
+              </Button>
+              <Button
+                className="my-1"
+                as={NavLink}
+                to={`${PREFIX}/settings`}
+                exact
+              >
+                Beállítások
+              </Button>
+              <Button className="my-1" onClick={onLogout}>
+                Kijelentkezés <i className="fas fa-sign-out-alt"></i>
+              </Button>
+            </>
+          ) : (
+            <Fragment>
+              <Button
+                className="my-1"
+                as={NavLink}
+                to={`${PREFIX}/register`}
+                exact
+              >
+                Regisztráció <i class="far fa-plus-square"></i>
+              </Button>
+              <Button
+                className="my-1"
+                as={NavLink}
+                to={`${PREFIX}/login`}
+                exact
+              >
+                Bejelentkezés <i class="fas fa-sign-in-alt"></i>
+              </Button>
+            </Fragment>
+          )}
+        </ButtonGroup>
       </div>
     </nav>
   );
