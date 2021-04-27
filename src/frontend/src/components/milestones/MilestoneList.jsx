053834ec5e14e5e9d9140a449094573f978997cf
zsolti1012 <56861547+zsolti1012@users.noreply.github.com>
2021-04-27 15:31:07 +0200

    Create list for Milestone

---
+++ src/frontend/src/components/milestones/MilestoneList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+import PropTypes from "prop-types";
+import MilestoneListItem from "./MilestoneListItem";
+
+const MilestoneList = ({ milestones }) => {
+  return (
+    <Table striped bordered hover className="no-vertical-border">
+      <thead>
+        <tr>
+          <th>Név</th>
+          <th>Típus</th>
+          <th>Szín</th>
+          <th>Egyenleg</th>
+          <th>Lehetőségek</th>
+        </tr>
+      </thead>
+      <tbody>
+        {milestones.map((milestone) => (
+          <MilestoneListItem key={milestone._id} milestone={milestone} />
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+MilestoneList.propTypes = {
+  milestones: PropTypes.array.isRequired,
+};
+
+export default MilestoneList;
